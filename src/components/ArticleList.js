0b8cf7a76dfb21edb65fe5faf9b61e462a3137ec
Daniel <muchiridaniel59@gmail.com>
2023-01-20 00:46:28 -0500

    completed assignment

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -11,3 +11,21 @@ function ArticleList (idMeal) {
 }
  
 export default ArticleList
+
+// const ArticleList = ({articles, mealTitle}) => {
+    
+
+//     return (
+//         <div className="article-List">
+//             <h2>{mealTitle}</h2>
+//              {articles.map((articles) =>(
+//                 <div className='articles-preview' key={articles.idMeal}>
+//                     <h2>{articles.mealTitle}</h2>
+//                     <p>{articles.description}</p>
+//                     <img>{articles.image}</img>
+//                 </div>
+//             ))}         
+//         </div>
+//     );
+//  }
+// export default ArticleList
