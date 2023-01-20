dd895500f40e814663cf637b287cc0d58709cd94
Daniel <muchiridaniel59@gmail.com>
2023-01-20 00:21:32 -0500

    completed

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // import React from 'react';
 
 
@@ -10,27 +9,7 @@
 // }
 
 // export default ArticleList;
-const ArticleList = ({articles, mealTitle}) => {
-    //const BlogList = (props)
-    // const blogs = props.blogs;
-    // const title = props.title;
 
-    
-
-    return (
-        <div className="article-List">
-            <h2>{mealTitle}</h2>
-             {articles.map((articles) =>(
-                <div className='articles-preview' key={articles.idMeal}>
-                    <h2>{articles.mealTitle}</h2>
-                    <p>{articles.description}</p>
-                    <img>{articles.image}</img>
-                </div>
-            ))}         
-        </div>
-    );
-=======
-import React from 'react';
 //import Article from 
 
 
@@ -43,7 +22,6 @@ function ArticleList (idMeal) {
             <Article key = {idMeal}/>
         </main>
     )
->>>>>>> 4784f5e8adbab45a0ed89d611495e5c0af0dfddd
 }
  
 export default ArticleList
