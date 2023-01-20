e9c4e0aeca10358557241a7498ae8300f86df3de
austinjnr <austinobonyo@gmail.com>
2023-01-20 06:14:51 +0300

    imported all files to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
 //import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import Data from '../blog-data/BlogData'
+import Data from '../blog-data/BlogData';
+//import Header from
+//import Article from 
+import ArticleList from './ArticleList'
+
 
 
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
+      <Article/>
+      <ArticleList/>
     </div>
   );
 }
